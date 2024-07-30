33b788ded731748e59170f4a0a56499ad7de4bd7
Liu Xiaoyi <circuitcoder0@gmail.com>
2024-07-31 00:57:54 +0800

    Multi-request DRAM access

--- src/mem.rs
+++ src/mem.rs
@@ -16,7 +16,7 @@ pub struct MemResp<const WIDTH: usize> {
 
 pub trait DelaySimulator {
   fn tick(&mut self);
-  fn push(&mut self, addr: u64, is_write: bool) -> bool;
+  fn push(&mut self, addr: u64, is_write: bool);
   fn pop(&mut self) -> Option<u64>;
 }
 
@@ -27,55 +27,133 @@ pub struct NoDelay {
 
 impl DelaySimulator for NoDelay {
   fn tick(&mut self) {}
-  fn push(&mut self, addr: u64, _is_write: bool) -> bool {
+  fn push(&mut self, addr: u64, _is_write: bool) {
     self.queue.push_back(addr);
-    true
   }
+
   fn pop(&mut self) -> Option<u64> {
     self.queue.pop_front()
   }
 }
 
-pub struct DRAMSim {
-  sys: dramsim3::MemorySystem,
-  channel: Rc<RefCell<VecDeque<u64>>>
+struct AddrProgress {
+  sent: u64,
+  recv: u64,
+  is_write: bool,
 }
 
-impl DRAMSim {
+impl AddrProgress {
+  fn next_send(&self, base: u64, transfer: u64) -> u64 {
+    base + transfer * self.sent
+  }
+}
+
+struct Progress<const WIDTH: usize> {
+  transfer_width: u64,
+
+  progress: HashMap<u64, AddrProgress>,
+  done: VecDeque<u64>,
+}
+
+impl<const WIDTH: usize> Default for Progress<WIDTH> {
+  fn default() -> Self {
+    Progress {
+      transfer_width: WIDTH as u64,
+      progress: HashMap::new(),
+      done: VecDeque::new(),
+    }
+  }
+}
+
+impl<const WIDTH: usize> Progress<WIDTH> {
+  fn add(&mut self, addr: u64, is_write: bool) {
+    assert_eq!(addr % (WIDTH as u64), 0);
+    assert!(self.progress.insert(addr, AddrProgress {
+      sent: 0,
+      recv: 0,
+      is_write,
+    }).is_some());
+  }
+
+  fn step(&mut self, addr: u64) {
+    let aligned = addr - addr % WIDTH as u64;
+    let multiplicity = self.multiplicity();
+    match self.progress.entry(aligned) {
+        std::collections::hash_map::Entry::Occupied(mut o) => {
+          let prog = o.get_mut();
+          assert_eq!(aligned + prog.recv * self.transfer_width, addr); // Sequential response
+          if prog.recv == multiplicity - 1 {
+            o.remove();
+            self.done.push_back(aligned);
+          } else {
+            prog.recv += 1;
+          }
+        }
+        std::collections::hash_map::Entry::Vacant(_) => panic!("Unexpected memory response"),
+    }
+  }
+
+  fn pop(&mut self) -> Option<u64> {
+    self.done.pop_front()
+  }
+
+  fn multiplicity(&self) -> u64 {
+    WIDTH as u64 / self.transfer_width
+  }
+}
+
+pub struct DRAMSim<const WIDTH: usize> {
+  sys: dramsim3::MemorySystem,
+  prog: Rc<RefCell<Progress<WIDTH>>>
+}
+
+impl<const WIDTH: usize> DRAMSim<WIDTH> {
   pub fn new<Config: AsRef<Path>, Dir: AsRef<Path>>(config: Config, dir: Dir) -> Self {
-    let channel: Rc<RefCell<VecDeque<u64>>> = Default::default();
-    let channel_cb = channel.clone();
+    let prog: Rc<RefCell<Progress<WIDTH>>> = Default::default();
+    let prog_cb = prog.clone();
 
     let config_cstr = CString::new(config.as_ref().as_os_str().as_encoded_bytes()).unwrap();
     let dir_cstr = CString::new(dir.as_ref().as_os_str().as_encoded_bytes()).unwrap();
     let sys = dramsim3::MemorySystem::new(&config_cstr, &dir_cstr, move |addr, _is_write| {
-      channel_cb.borrow_mut().push_back(addr)
+      prog_cb.borrow_mut().step(addr)
     });
-    DRAMSim { sys, channel }
+
+    let transfer_width = sys.bus_bits() * sys.burst_length() / 8;
+    prog.borrow_mut().transfer_width = transfer_width as u64;
+
+    DRAMSim { sys, prog }
   }
 }
 
-impl DelaySimulator for DRAMSim {
+impl<const WIDTH: usize> DelaySimulator for DRAMSim<WIDTH> {
   fn tick(&mut self) {
-    self.sys.tick()
+    self.sys.tick();
+    let mut prog = self.prog.borrow_mut();
+    let multiplicity = prog.multiplicity();
+    let transfer_width = prog.transfer_width;
+    for (aligned, addr_prog) in prog.progress.iter_mut() {
+      if addr_prog.sent != multiplicity {
+        let next_addr = addr_prog.next_send(*aligned, transfer_width);
+        if self.sys.can_add(next_addr, addr_prog.is_write) {
+          self.sys.add(next_addr, addr_prog.is_write);
+          addr_prog.sent += 1;
+        }
+      }
+    }
   }
 
-  fn push(&mut self, addr: u64, is_write: bool) -> bool {
-    if !self.sys.can_add(addr, is_write) {
-      return false;
-    }
-    self.sys.add(addr, is_write)
+  fn push(&mut self, addr: u64, is_write: bool) {
+    self.prog.borrow_mut().add(addr, is_write);
   }
 
   fn pop(&mut self) -> Option<u64> {
-    self.channel.borrow_mut().pop_front()
+    self.prog.borrow_mut().pop()
   }
 }
 
 pub struct Mem<D: DelaySimulator, const WIDTH: usize> {
   sim: D,
   content: HashMap<u64, [u8; WIDTH]>,
-  pendings: VecDeque<MemReq<WIDTH>>,
   inflights: HashMap<u64, usize>,
 }
 
@@ -84,7 +162,6 @@ impl<D: DelaySimulator, const WIDTH: usize> Mem<D, WIDTH> {
     Mem {
       sim,
       content: HashMap::new(),
-      pendings: VecDeque::new(),
       inflights: HashMap::new(),
     }
   }
@@ -95,34 +172,27 @@ impl<D: DelaySimulator, const WIDTH: usize> Drain for Mem<D, WIDTH> {
   type Resp = MemResp<WIDTH>;
   fn tick(&mut self) {
     self.sim.tick();
-
-    while let Some(req) = self.pendings.front() {
-      // TODO: check addr alignment
-      if self.sim.push(req.addr, req.wbe.contains(&true)) {
-        match self.content.entry(req.addr) {
-          std::collections::hash_map::Entry::Occupied(mut o) => {
-            for (c, (w, be)) in o.get_mut().iter_mut().zip(req.wdata.iter().zip(req.wbe.iter())) {
-              if *be { *c = *w; }
-            }
-          },
-          std::collections::hash_map::Entry::Vacant(v) => {
-            let mut buf = [0; WIDTH];
-            for (c, (w, be)) in buf.iter_mut().zip(req.wdata.iter().zip(req.wbe.iter())) {
-              if *be { *c = *w; }
-            }
-            v.insert(buf);
-          },
-        }
-        self.pendings.pop_front();
-      }
-    }
   }
 
   fn push(&mut self, req: MemReq<WIDTH>) {
     if self.inflights.insert(req.addr, req.id).is_some() {
       panic!("Duplicated inflight memory requests");
     }
-    self.pendings.push_back(req);
+    self.sim.push(req.addr, req.wbe.contains(&true));
+    match self.content.entry(req.addr) {
+      std::collections::hash_map::Entry::Occupied(mut o) => {
+        for (c, (w, be)) in o.get_mut().iter_mut().zip(req.wdata.iter().zip(req.wbe.iter())) {
+          if *be { *c = *w; }
+        }
+      },
+      std::collections::hash_map::Entry::Vacant(v) => {
+        let mut buf = [0; WIDTH];
+        for (c, (w, be)) in buf.iter_mut().zip(req.wdata.iter().zip(req.wbe.iter())) {
+          if *be { *c = *w; }
+        }
+        v.insert(buf);
+      },
+    }
   }
 
   fn pop(&mut self) -> Option<MemResp<WIDTH>> {
